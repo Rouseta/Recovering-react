143d9ac12d3883b458cac1db38a0888bf03fa7ea
Rouseta <catacrocken@gmail.com>
2025-06-16 11:56:45 +0200

    adding new props to format the user name

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -2,11 +2,33 @@ import '../assets/App.css';
 import { TwitterFollowCard } from './TwitterFollowCard';
 
 export function App() {
+  const formatUserName = (userName) => `@${userName}`;
   return (
-    <section className='App'>
-      <TwitterFollowCard userName="Rouseta" name="Rosa Donoso Requena" />
-      <TwitterFollowCard userName="Puch" name="Juan Roch González" />
-       <TwitterFollowCard userName="Dumbledore" name="Albus Dumbledore" />
+    <section className="App">
+      <TwitterFollowCard
+        isFollowing
+        userName="Rouseta"
+        name="Rosa Donoso Requena"
+        formatUserName={formatUserName}
+      />
+      <TwitterFollowCard
+        isFollowing
+        userName="Puch"
+        name="Juan Roch González"
+        formatUserName={formatUserName}
+      />
+      <TwitterFollowCard
+        isFollowing
+        userName="Dumbledore"
+        name="Albus Dumbledore"
+        formatUserName={formatUserName}
+      />
+      <TwitterFollowCard
+        isFollowing={false}
+        userName="Messi"
+        name="Leo Messi"
+        formatUserName={formatUserName}
+      />
     </section>
   );
 }
--- src/assets/TwitterFollowCard.jsx
+++ src/assets/TwitterFollowCard.jsx
@@ -1,16 +1,27 @@
-export function TwitterFollowCard({userName, name}){
-    return(
-     <article className='tw-followCard'>
-      <header className='tw-followCard-header'>
-        <img className='tw-followCard-avatar'src={`https://unavatar.io/${userName}`} alt="El avatar de Rosa " />
-        <div className='tw-followCard-info'>
+export function TwitterFollowCard({
+  formatUserName,
+  userName,
+  name,
+  isFollowing,
+}) {
+  return (
+    <article className="tw-followCard">
+      <header className="tw-followCard-header">
+        <img
+          className="tw-followCard-avatar"
+          src={`https://unavatar.io/${userName}`}
+          alt="El avatar de Rosa "
+        />
+        <div className="tw-followCard-info">
           <strong>{name}</strong>
-          <span className='tw-followCard-infoUserName'>@{userName}</span>
+          <span className="tw-followCard-infoUserName">
+            {formatUserName(userName)}
+          </span>
         </div>
       </header>
       <aside>
-        <button className='tw-followCard-button '>Seguir</button>
+        <button className="tw-followCard-button ">Seguir</button>
       </aside>
     </article>
-    )
+  );
 }
