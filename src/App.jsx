96f909ce18288503f4b93afde43e4a4527764351
Rouseta <catacrocken@gmail.com>
2025-07-02 12:40:13 +0200

    adding states

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
+import { useState } from 'react';
 import './App.css';
 const TURNS = {
   X: 'x',
   O: 'o',
 };
-const board = Array(9).fill(null);
 
+const Square = ({ children, updateBoard, index, isSelected }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`;
+
+  return <div className={className}>{children}</div>;
+};
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
   return (
     <main className="board">
       <h1>Tic tac toe</h1>
       <section className="game">
         {board.map((_, index) => {
           return (
-            <div className="cell" key={index}>
-              <span className="cell__content">{index}</span>
-            </div>
+            <Square key={index} index={index}>
+              {board[index]}
+            </Square>
           );
         })}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
     </main>
   );
