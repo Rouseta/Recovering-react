71bcf0dc38f73d49ec3aea641256928e051ed43c
Rouseta <catacrocken@gmail.com>
2025-07-08 12:28:47 +0200

    adding handle click to handle the turn

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,32 @@ const TURNS = {
 
 const Square = ({ children, updateBoard, index, isSelected }) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`;
+  const handleClick = () => {
+    updateBoard();
+  };
 
-  return <div className={className}>{children}</div>;
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
 };
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
+  const updateBoard = () => {
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+  };
+
   return (
     <main className="board">
       <h1>Tic tac toe</h1>
       <section className="game">
         {board.map((_, index) => {
           return (
-            <Square key={index} index={index}>
+            <Square key={index} index={index} updateBoard={updateBoard}>
               {board[index]}
             </Square>
           );
