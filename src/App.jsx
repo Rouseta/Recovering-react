c63c4449247d73468019d0264b25d8e9153bf60e
Rouseta <catacrocken@gmail.com>
2025-12-03 16:34:59 +0100

    add new variable to stablish the winner

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,22 @@ const Square = ({ children, updateBoard, index, isSelected }) => {
   );
 };
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null); //null no hay ganador, false es empate
+
   const updateBoard = (index) => {
     // If the square is already filled, do nothing
     if (board[index]) return;
